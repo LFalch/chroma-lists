bcf703d3b43677fc79894b3b498a12c73b4442e5
LFalch <lucas@wasd.dk>
2017-10-20 17:31:38 +0200

    Use clap to parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
+extern crate clap;
 extern crate chroma_lists;
+
+use std::fs::File;
+use std::io::Write;
+use std::time::Instant;
+
+use clap::{App, Arg};
 
 use chroma_lists::ChromaList;
 
-use std::env::args;
-use std::time::Instant;
-
 fn main() {
-    let s = args().skip(1).collect::<Vec<_>>().join(" ");
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+                          .version(env!("CARGO_PKG_VERSION"))
+                          .author(env!("CARGO_PKG_AUTHORS"))
+                          .about(env!("CARGO_PKG_DESCRIPTION"))
+                          .arg(Arg::with_name("SEQUENCE")
+                               .help("Sequence to run through algorithm")
+                               .multiple(true)
+                               .required(true)
+                               .takes_value(true))
+                          .arg(Arg::with_name("steps")
+                               .short("f")
+                               .long("steps-file")
+                               .takes_value(true)
+                               .help("File to output all the intermediate steps into"))
+                          .get_matches();
+    let mut output_file = matches.value_of("steps").map(|p| File::create(p).unwrap());
+
+    let s = matches.values_of("SEQUENCE").unwrap().collect::<Vec<_>>().join(" ");
 
     let list: ChromaList = s.trim().parse().unwrap();
     let now = Instant::now();
-    let iters = list.into_iter().count();
+    let iters = list.into_iter().inspect(|s| if let Some(ref mut o) = output_file {write!(o, "{}", s).unwrap()}).count();
     let duration = Instant::now() - now;
 
     println!("{} iterations. Took {}.{:09}s", iters, duration.as_secs(), duration.subsec_nanos());
